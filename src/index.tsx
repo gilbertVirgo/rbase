7e5058d14e836c7b44f6af635d410275ec0a426c
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-10-24 15:58:53 -0400

    patch

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { all, parse } from "styluss";
+
+interface ImageProps {
+    src?: string,
+    alt?: string
+}
+
+export const Text = (props: { children?: string } & all) => {
+    return <p style={parse(props)}>{props.children}</p>
+}
+
+export const View = (props: { children?: JSX.Element | JSX.Element[] } & all) => {
+    return <div style={parse(props)}>{props.children}</div>
+}
+
+export const Image = (props: ImageProps & all) => {
+    return <img src={props.src} alt={props.alt} style={parse(props)} />
+}
