09687ba8af000b1ef69ccdab5b11036fd86d2418
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-10-25 00:16:37 -0400

    patch

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
 import React from "react";
 import { all, parse } from "styluss";
 
-interface ImageProps {
+interface TextProps extends all {
+    children?: JSX.Element | JSX.Element[] | string // Include elements in the case of <b>, <i>, etc.
+}
+
+interface DefaultProps extends all {
+    children?: JSX.Element | JSX.Element[]
+}
+
+interface ImageProps extends all {
     src?: string,
     alt?: string
 }
 
-export const Text = (props: { children?: string } & all) => {
-    return <p style={parse(props)}>{props.children}</p>
+interface ButtonProps extends DefaultProps {
+    onPointerDown?: any,
+    onPointerUp?: any,
+    onClick?: any,
+    onTouchStart?: any,
+    onTouchEnd?: any
 }
 
-export const View = (props: { children?: JSX.Element | JSX.Element[] } & all) => {
-    return <div style={parse(props)}>{props.children}</div>
-}
+export const Text = (props: TextProps) => (
+    <p style={parse(props)}>{props.children}</p>
+);
 
-export const Image = (props: ImageProps & all) => {
-    return <img src={props.src} alt={props.alt} style={parse(props)} />
-}
+export const View = (props: DefaultProps) => (
+    <div style={parse(props)}>{props.children}</div>
+);
 
+export const Image = (props: ImageProps) => (
+    <img style={parse(props)} src={props.src} alt={props.alt} />
+);
+
+export const Button = (props: ButtonProps) => (
+    <div
+        style={parse(props)}
+        onPointerDown={props.onPointerDown}
+        onPointerUp={props.onPointerUp}
+        onClick={props.onClick}
+        onTouchStart={props.onTouchStart}
+        onTouchEnd={props.onTouchEnd}>
+        {props.children}
+    </div>
+);
